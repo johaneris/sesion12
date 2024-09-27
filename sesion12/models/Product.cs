8f71f501889f1bdaa83cae6cd227b834b1b3a8c4
johaneris <jsavalos@uamv.edu.ni>
2024-09-27 08:32:15 -0600

    agregue la sestructura

---
+++ sesion12/models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sesion12.models
+{
+    public struct Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double price { get; set; }
+
+    }
+}
